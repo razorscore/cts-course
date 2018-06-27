2c218670ed06e9c2cb3d7dc9c172365f5d98e46d
Daven Triplett <daventriplett@gmail.com>
2018-06-27 08:59:48 -0600

    API errors and created ModuleSection

---
+++ ModuleSection/src/bootstrap.js
@@ -0,0 +1,4 @@
+import * as helper from './helper';
+
+console.log(helper.greeting);
+console.log(helper.multiply(5, 20));
---
+++ ModuleSection/src/helper.js
@@ -0,0 +1,5 @@
+export function multiply(numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+export const greeting = 'Hi there';
